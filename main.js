64032a09a59c97439658c995820df5efbc5d4320
Daviixo <daviidvx93@gmail.com>
2024-02-13 19:16:39 -0600

    First commit. Almost ready!

---
+++ main.js
@@ -0,0 +1,24 @@
+let clickCount = 0;
+
+function handleNoButtonClick() {
+    let btnNo = document.getElementById("btn_no");
+    let currentSize = parseFloat(window.getComputedStyle(btnNo).getPropertyValue("font-size"));
+    let newSize = currentSize * 0.8; // Reduce el tamaño actual en un 20%
+
+    btnNo.style.fontSize = newSize + "px";
+
+    clickCount++;
+
+    if (clickCount === 5) {
+        btnNo.style.display = "none";
+        alert("¡Qué maldad! ¿Por qué dices que NO? :(");
+    }
+}
+
+function handleYesButtonClick() {
+    document.getElementById("content").innerHTML = '<img src="http://tinyurl.com/2xzxmdn4" alt="GIF">' 
+    + '<br><img src="http://tinyurl.com/292a6qut" alt="Image">' + '<br><h1>Te amo, amorcito! ❤️</h1><h4>Gracias por no decir que no :3 Tu regalo está pendiente... #Sorpresitas</h4>';
+}
+
+document.getElementById("btn_no").addEventListener("click", handleNoButtonClick);
+document.getElementById("btn_yes").addEventListener("click", handleYesButtonClick);
